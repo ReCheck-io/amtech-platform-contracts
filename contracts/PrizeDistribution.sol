56671ee0889dd1f40c4bb59cb19c3a816a979931
Ventsislav Tsochev <ventsi@limechain.tech>
2020-07-29 09:11:43 +0300

    dev comments ans small naming fixes

--- contracts/PrizeDistribution.sol
+++ contracts/PrizeDistribution.sol
@@ -4,6 +4,14 @@ import "./interfaces/IRandomGenerator.sol";
 import "../node_modules/@openzeppelin/contracts/math/SafeMath.sol";
 import "../node_modules/@openzeppelin/contracts/access/Ownable.sol";
 
+/**
+ * @dev {PrizeDistribution Contract}:
+ *
+ *  This contract holds token holder addresses and their balances as weights and
+ *provides the functionality to draw winners based on those weigths.
+ *
+ */
+
 contract PrizeDistribution is Ownable {
     using SafeMath for uint256;
 
@@ -24,32 +32,52 @@ contract PrizeDistribution is Ownable {
         bool isActiv;
     }
 
+    /**
+     * @dev Set random number generator contract
+     *
+     */
     constructor(address _randomGenerator) public {
         randomGenerator = IRandomGenerator(_randomGenerator);
     }
 
     // TODO: should set max number of winners per draw
-    function drawWinners(uint256 n, uint256 userSeed) public returns (bool) {
+    /**
+     * @dev Draw _n number of winners based on verifiable random number, seed word and user weights and
+     * add them to a list of winners per round
+     *
+     * Requirements:
+     * - Amtech token must be paused
+     *
+     * Can be called from any user
+     */
+    function drawWinners(uint256 _n, uint256 userSeed) public returns (bool) {
         uint256 winningPosition;
         uint256 random = randomGenerator.rollTheDice(userSeed);
-        for (uint256 i = 0; i < n; i++) {
+        for (uint256 i = 0; i < _n; i++) {
             winningPosition = (
-                tokenHolderWeights[((random * i) % tokenHolders.length)]
+                // TODO: (i + 1)?
+                tokenHolderWeights[(random * (i + 1)) % tokenHolders.length]
             );
             roundsAndWinningPositions[currentRound].push(winningPosition);
 
             address winner = findWinner(winningPosition);
             currentRoundWinnersAddress.push(winner);
         }
+        return true;
     }
 
     // TODO: is it possible for one user to win multiple prices
-    function findWinner(uint256 RANDOM) public view returns (address) {
+    /**
+     * @dev Uses a weighted random algorithm to find the winner.
+     * Returns the winning address
+     *
+     */
+    function findWinner(uint256 winningPosition) public view returns (address) {
         uint256 drawedBalances = 0;
         for (uint256 i = 0; i < tokenHolderWeights.length; i++) {
             drawedBalances = drawedBalances.add(tokenHolderWeights[i]);
 
-            if (drawedBalances > RANDOM) {
+            if (drawedBalances > winningPosition) {
                 return tokenHolders[i];
             }
         }
@@ -57,6 +85,12 @@ contract PrizeDistribution is Ownable {
 
     // TODO: Token contract should set this
     // TODO: some validations maybe
+    /**
+     * @dev Set token holder addresses and their balances as weights.
+     * Requirements:
+     * - should be executable only from token contract
+     *
+     */
     function setUserWheight(address tokenHolder, uint256 weight) public {
         if (!indexToHolderAddress[tokenHolder].isActiv) {
             tokenHolders.push(tokenHolder);
