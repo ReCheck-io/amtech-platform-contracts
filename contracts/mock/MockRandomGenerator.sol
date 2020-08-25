2c59972d348c12fa3ec4735a391892cbdc042176
Kristian Veselinov <thcrnk@gmail.com>
2020-08-25 08:42:14 +0300

    Run the ganache in background

--- contracts/mock/MockRandomGenerator.sol
+++ contracts/mock/MockRandomGenerator.sol
@@ -3,7 +3,8 @@ pragma solidity ^0.6.6;
 import "../interfaces/IRandomGenerator.sol";
 import "../../node_modules/@openzeppelin/contracts/math/SafeMath.sol";
 
-contract mockRandomGenerator is IRandomGenerator {
+
+contract MockRandomGenerator is IRandomGenerator {
     using SafeMath for uint256;
 
     function rollTheDice(uint256 userProvidedSeed)
